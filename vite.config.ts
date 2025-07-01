f295354bbe3d82469f93eb427ba7b9dc92792d70
noorhanooot <noor@hanooot.com>
2025-07-01 12:18:50 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+   build: {
+    rollupOptions: {
+      external: ['/src/main.tsx'] // <-- Add this line if absolutely necessary
+    }
+  }
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
